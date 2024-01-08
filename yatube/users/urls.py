5e4fbc215929444d9aef46d345fa653a748e2fae
Илья Василевский <vasilevskijila047@gmail.com>
2024-01-08 18:52:27 +0300

    Предпоследнее обновление

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,8 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView,
+    PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import path
 
 from . import views
@@ -19,32 +23,44 @@ urlpatterns = [
     ),
     path(
         "password_change/",
-        LoginView.as_view(template_name="users/password_change_form.html"),
+        PasswordChangeView.as_view(
+            template_name="users/password_change_form.html"
+        ),
         name="password_change",
     ),
     path(
         "password_change/done/",
-        LoginView.as_view(template_name="users/password_change_done.html"),
+        PasswordChangeDoneView.as_view(
+            template_name="users/password_change_done.html"
+        ),
         name="password_change_done",
     ),
     path(
         "password_reset/",
-        LoginView.as_view(template_name="users/password_reset_form.html"),
+        PasswordResetView.as_view(
+            template_name="users/password_reset_form.html"
+            ),
         name="password_reset",
     ),
     path(
         "password_reset/done/",
-        LoginView.as_view(template_name="users/password_reset_done.html"),
+        PasswordResetDoneView.as_view(
+            template_name="users/password_reset_done.html"
+        ),
         name="password_reset_done",
     ),
     path(
-        "reset/<uid64>/<token>/",
-        LoginView.as_view(template_name="users/password_reset_confirm.html"),
-        name="reset_confirm",
+        "reset/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"
+        ),
+        name="password_reset_confirm",
     ),
     path(
-        "reser/done/",
-        LoginView.as_view(template_name="users/password_reset_complete.html"),
-        name="reset_complete",
+        "reset/done/",
+        PasswordResetCompleteView.as_view(
+            template_name="users/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -129,6 +129,12 @@ LOGIN_REDIRECT_URL = 'posts:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+# MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'test@gmail.com'
+# EMAIL_HOST_PASSWORD = '123'
 
 # Caches
 
